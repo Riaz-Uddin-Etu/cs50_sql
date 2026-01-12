d4c8a3b2b31c4e0d2b00c12bb7039d5513c9f66c
Riaz-Uddin-Etu <riazuddinetu23@gmail.com>
2026-01-12 16:43:52 +0600

    Add 3 new sql queries

--- week_1_relating/dese.sql
+++ week_1_relating/dese.sql
@@ -70,3 +70,39 @@ ON "districts"."id" = "expenditures"."district_id"
 WHERE "districts"."type" = 'Public School District'
 ORDER BY "expenditures"."per_pupil_expenditure" DESC
 LIMIT 10;
+
+-- In 11.sql, write a SQL query to display the names of schools, their per-pupil expenditure, and their graduation rate.
+-- Sort the schools from greatest per-pupil expenditure to least. If two schools have the same per-pupil expenditure, sort by school name.
+-- You should assume a school spends the same amount per-pupil their district as a whole spends.
+SELECT "schools"."name", "expenditures"."per_pupil_expenditure", "graduation_rates"."graduated"
+FROM "schools"
+JOIN "graduation_rates"
+ON "schools"."id" = "graduation_rates"."school_id"
+JOIN "expenditures"
+ON "schools"."district_id" = "expenditures"."district_id"
+ORDER BY "expenditures"."per_pupil_expenditure" DESC, "schools"."name"
+
+-- In 12.sql, write a SQL query to find public school districts with above-average per-pupil expenditures and an above-average percentage of teachers rated “exemplary”.
+-- Your query should return the districts’ names, along with their per-pupil expenditures and percentage of teachers rated exemplary.
+-- Sort the results first by the percentage of teachers rated exemplary (high to low), then by the per-pupil expenditure (high to low).
+SELECT "d"."name", "e"."per_pupil_expenditure", "s"."exemplary"
+FROM "districts" AS "d"
+JOIN "expenditures" AS "e"
+ON "d"."id" = "e"."district_id"
+JOIN "staff_evaluations" AS "s"
+ON "d"."id" = "s"."district_id"
+WHERE "e"."per_pupil_expenditure" > (
+    SELECT AVG("per_pupil_expenditure")
+    FROM "expenditures"
+) AND "s"."exemplary" > (
+    SELECT AVG("exemplary")
+    FROM "staff_evaluations"
+) AND "d"."type" = 'Public School District'
+ORDER BY "s"."exemplary" DESC, "e"."per_pupil_expenditure" DESC;
+
+-- In 13.sql, write a SQL query to answer a question you have about the data! The query should:
+-- Involve at least one JOIN or subquery
+SELECT "d"."name", ("e"."pupils" * "e"."per_pupil_expenditure") AS "total_expenditure_on_pupil"
+FROM "districts" AS "d"
+JOIN "expenditures" AS "e"
+ON "d"."id" = "e"."district_id"
